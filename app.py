102bd1a6f8b13785d05da39596b065238a2d5361
John Garner <66663652+JohnGarnerIII@users.noreply.github.com>
2023-05-26 10:51:36 -0400

    added title

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask
-def create_app():
-    app = Flask(__name__)
-    @app.route('/')
-    def hello_world():
-        return 'Hello, World!'
-    return app
-app = create_app()
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return """
+    <html>
+    <body>
+        <h1 style="text-align:center;">Chen Bible</h1>
+    </body>
+    </html>
+    """
+
+if __name__ == '__main__':
+    app.run(debug=True)
